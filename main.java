fc250aaade536b505a44d5fc634dec5eba3d1054
timur <tmrmkrmv@mail.ru>
2024-09-04 03:06:15 +0500

    Operations
    Простые операции в Java

--- main.java
+++ main.java
@@ -2,6 +2,12 @@ public class main {
     public static void main (String[] args) {
 
         System.out.println("Hello World");
+        System.out.println(1+1);
+        System.out.println(1-1);
+        System.out.println(2.0/3);
+        System.out.println(2*7);
+        System.out.println("1321" + "23");
+
 
     }
 }
